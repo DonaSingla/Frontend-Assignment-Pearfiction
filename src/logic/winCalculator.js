fbb57bbed4ca9a6d295f8640c2dec47449beb547
Dona Singla <dsingla42@outlook.com>
2026-06-18 20:42:01 +0530

    Add comments for winCalculator  functions

--- src/logic/winCalculator.js
+++ src/logic/winCalculator.js
@@ -1,4 +1,16 @@
 import { PAYLINES, PAYTABLE } from '../config/gameConfig.js';
+
+/*
+Example: checkPayline(PAYLINES[0], screen)
+
+- PAYLINES[0] is the middle row (1,0), (1,1), (1,2), (1,3), (1,4)
+- firstSymbol = screen[1][0]
+- Loop through positions:
+  - If symbols match → count++
+  - If mismatch → break
+- If count ≥ 3 → return win object
+*/
+
 export function checkPayline(payline, screen) {
     const firstSymbol = screen[payline.positions[0][0]][payline.positions[0][1]];
     let count = 0;
@@ -17,6 +29,20 @@ export function checkPayline(payline, screen) {
         payout: PAYTABLE[firstSymbol][count],
     };
 }
+
+/* calculateWins(screen)
+
+ calls checkPayline() for each of the 7 paylines
+
+ collect all non-null results
+
+ Example output:
+ [
+  { paylineId: 2, symbol: 'hv2', count: 3, payout: 5 },
+  { paylineId: 5, symbol: 'lv3', count: 3, payout: 1 }
+]
+
+*/
 export function calculateWins(screen) {
     const wins = [];
     for (const payline of PAYLINES) {
